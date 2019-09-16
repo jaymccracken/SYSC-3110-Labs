85db2222e840da3029d92e046c586069fbe62024
Jaymccracken <Jaymccracken@cb5109-20.labs.sce.carleton.ca>
2019-09-16 12:09:22 -0400

    Added some demo code to the main function

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -15,4 +15,11 @@ public class AddressBook {
 			buddyInfo.remove(index);
 		}
 	}
+	
+	public static void main(String[] args) {
+		BuddyInfo buddy = new BuddyInfo("Tom","Carleton","613");
+		AddressBook addressBook = new AddressBook();
+		addressBook.addBuddy(buddy);
+		addressBook.removeBuddy(0);
+	}
 }
